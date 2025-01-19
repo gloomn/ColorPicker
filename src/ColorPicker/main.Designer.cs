8de2500192eefeca7b4b16a4ac8b5b86e5a4ad5f
gloomn <ithan0704@naver.com>
2025-01-19 20:27:40 +0900

    - design

--- src/ColorPicker/main.Designer.cs
+++ src/ColorPicker/main.Designer.cs
@@ -28,20 +28,317 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main));
+            this.screen_color = new System.Windows.Forms.PictureBox();
+            this.screen = new System.Windows.Forms.PictureBox();
+            this.xPosition = new System.Windows.Forms.Label();
+            this.yPosition = new System.Windows.Forms.Label();
+            this.rgbText = new System.Windows.Forms.Label();
+            this.copyright = new System.Windows.Forms.Label();
+            this.hexText = new System.Windows.Forms.Label();
+            this.selected_B = new System.Windows.Forms.Label();
+            this.color_list = new System.Windows.Forms.ListBox();
+            this.selected_G = new System.Windows.Forms.Label();
+            this.selected_color = new System.Windows.Forms.PictureBox();
+            this.selected_R = new System.Windows.Forms.Label();
+            this.hslText = new System.Windows.Forms.Label();
+            this.hsvText = new System.Windows.Forms.Label();
+            this.cmykText = new System.Windows.Forms.Label();
+            this.screen_timer = new System.Windows.Forms.Timer(this.components);
+            this.cielabText = new System.Windows.Forms.Label();
+            this.xyzText = new System.Windows.Forms.Label();
+            this.yuvText = new System.Windows.Forms.Label();
+            this.ycbcrText = new System.Windows.Forms.Label();
+            this.lchText = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.screen_color)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.screen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.selected_color)).BeginInit();
             this.SuspendLayout();
+            // 
+            // screen_color
+            // 
+            this.screen_color.Location = new System.Drawing.Point(169, 12);
+            this.screen_color.Name = "screen_color";
+            this.screen_color.Size = new System.Drawing.Size(138, 138);
+            this.screen_color.TabIndex = 24;
+            this.screen_color.TabStop = false;
+            this.screen_color.Paint += new System.Windows.Forms.PaintEventHandler(this.screen_color_Paint);
+            // 
+            // screen
+            // 
+            this.screen.Location = new System.Drawing.Point(12, 12);
+            this.screen.Name = "screen";
+            this.screen.Size = new System.Drawing.Size(138, 138);
+            this.screen.TabIndex = 23;
+            this.screen.TabStop = false;
+            this.screen.Paint += new System.Windows.Forms.PaintEventHandler(this.screen_Paint);
+            // 
+            // xPosition
+            // 
+            this.xPosition.AutoSize = true;
+            this.xPosition.BackColor = System.Drawing.Color.Transparent;
+            this.xPosition.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.xPosition.ForeColor = System.Drawing.Color.White;
+            this.xPosition.Location = new System.Drawing.Point(13, 153);
+            this.xPosition.Name = "xPosition";
+            this.xPosition.Size = new System.Drawing.Size(19, 17);
+            this.xPosition.TabIndex = 25;
+            this.xPosition.Text = "X:";
+            // 
+            // yPosition
+            // 
+            this.yPosition.AutoSize = true;
+            this.yPosition.BackColor = System.Drawing.Color.Transparent;
+            this.yPosition.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.yPosition.ForeColor = System.Drawing.Color.White;
+            this.yPosition.Location = new System.Drawing.Point(71, 153);
+            this.yPosition.Name = "yPosition";
+            this.yPosition.Size = new System.Drawing.Size(19, 17);
+            this.yPosition.TabIndex = 26;
+            this.yPosition.Text = "Y:";
+            // 
+            // rgbText
+            // 
+            this.rgbText.AutoSize = true;
+            this.rgbText.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rgbText.ForeColor = System.Drawing.Color.White;
+            this.rgbText.Location = new System.Drawing.Point(325, 17);
+            this.rgbText.Name = "rgbText";
+            this.rgbText.Size = new System.Drawing.Size(36, 17);
+            this.rgbText.TabIndex = 27;
+            this.rgbText.Text = "RGB:";
+            // 
+            // copyright
+            // 
+            this.copyright.AutoSize = true;
+            this.copyright.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.copyright.ForeColor = System.Drawing.Color.DarkGray;
+            this.copyright.Location = new System.Drawing.Point(3, 361);
+            this.copyright.Name = "copyright";
+            this.copyright.Size = new System.Drawing.Size(195, 13);
+            this.copyright.TabIndex = 36;
+            this.copyright.Text = " © 2025 LeeKiJoon all rights reserved";
+            // 
+            // hexText
+            // 
+            this.hexText.AutoSize = true;
+            this.hexText.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hexText.ForeColor = System.Drawing.Color.White;
+            this.hexText.Location = new System.Drawing.Point(325, 44);
+            this.hexText.Name = "hexText";
+            this.hexText.Size = new System.Drawing.Size(36, 17);
+            this.hexText.TabIndex = 30;
+            this.hexText.Text = "HEX:";
+            // 
+            // selected_B
+            // 
+            this.selected_B.AutoSize = true;
+            this.selected_B.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.selected_B.ForeColor = System.Drawing.Color.White;
+            this.selected_B.Location = new System.Drawing.Point(324, 297);
+            this.selected_B.Name = "selected_B";
+            this.selected_B.Size = new System.Drawing.Size(19, 17);
+            this.selected_B.TabIndex = 35;
+            this.selected_B.Text = "B:";
+            // 
+            // color_list
+            // 
+            this.color_list.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(29)))), ((int)(((byte)(39)))));
+            this.color_list.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.color_list.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.color_list.ForeColor = System.Drawing.Color.White;
+            this.color_list.FormattingEnabled = true;
+            this.color_list.ItemHeight = 17;
+            this.color_list.Location = new System.Drawing.Point(12, 190);
+            this.color_list.Name = "color_list";
+            this.color_list.Size = new System.Drawing.Size(147, 138);
+            this.color_list.TabIndex = 31;
+            // 
+            // selected_G
+            // 
+            this.selected_G.AutoSize = true;
+            this.selected_G.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.selected_G.ForeColor = System.Drawing.Color.White;
+            this.selected_G.Location = new System.Drawing.Point(324, 269);
+            this.selected_G.Name = "selected_G";
+            this.selected_G.Size = new System.Drawing.Size(20, 17);
+            this.selected_G.TabIndex = 34;
+            this.selected_G.Text = "G:";
+            // 
+            // selected_color
+            // 
+            this.selected_color.Location = new System.Drawing.Point(169, 190);
+            this.selected_color.Name = "selected_color";
+            this.selected_color.Size = new System.Drawing.Size(138, 138);
+            this.selected_color.TabIndex = 32;
+            this.selected_color.TabStop = false;
+            // 
+            // selected_R
+            // 
+            this.selected_R.AutoSize = true;
+            this.selected_R.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.selected_R.ForeColor = System.Drawing.Color.White;
+            this.selected_R.Location = new System.Drawing.Point(325, 237);
+            this.selected_R.Name = "selected_R";
+            this.selected_R.Size = new System.Drawing.Size(19, 17);
+            this.selected_R.TabIndex = 33;
+            this.selected_R.Text = "R:";
+            // 
+            // hslText
+            // 
+            this.hslText.AutoSize = true;
+            this.hslText.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hslText.ForeColor = System.Drawing.Color.White;
+            this.hslText.Location = new System.Drawing.Point(327, 71);
+            this.hslText.Name = "hslText";
+            this.hslText.Size = new System.Drawing.Size(34, 17);
+            this.hslText.TabIndex = 37;
+            this.hslText.Text = "HSL:";
+            // 
+            // hsvText
+            // 
+            this.hsvText.AutoSize = true;
+            this.hsvText.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hsvText.ForeColor = System.Drawing.Color.White;
+            this.hsvText.Location = new System.Drawing.Point(325, 98);
+            this.hsvText.Name = "hsvText";
+            this.hsvText.Size = new System.Drawing.Size(36, 17);
+            this.hsvText.TabIndex = 38;
+            this.hsvText.Text = "HSV:";
+            // 
+            // cmykText
+            // 
+            this.cmykText.AutoSize = true;
+            this.cmykText.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmykText.ForeColor = System.Drawing.Color.White;
+            this.cmykText.Location = new System.Drawing.Point(314, 125);
+            this.cmykText.Name = "cmykText";
+            this.cmykText.Size = new System.Drawing.Size(47, 17);
+            this.cmykText.TabIndex = 39;
+            this.cmykText.Text = "CMYK:";
+            // 
+            // screen_timer
+            // 
+            this.screen_timer.Tick += new System.EventHandler(this.screen_timer_Tick);
+            // 
+            // cielabText
+            // 
+            this.cielabText.AutoSize = true;
+            this.cielabText.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cielabText.ForeColor = System.Drawing.Color.White;
+            this.cielabText.Location = new System.Drawing.Point(504, 18);
+            this.cielabText.Name = "cielabText";
+            this.cielabText.Size = new System.Drawing.Size(53, 17);
+            this.cielabText.TabIndex = 40;
+            this.cielabText.Text = "CIELAB:";
+            // 
+            // xyzText
+            // 
+            this.xyzText.AutoSize = true;
+            this.xyzText.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.xyzText.ForeColor = System.Drawing.Color.White;
+            this.xyzText.Location = new System.Drawing.Point(522, 44);
+            this.xyzText.Name = "xyzText";
+            this.xyzText.Size = new System.Drawing.Size(35, 17);
+            this.xyzText.TabIndex = 41;
+            this.xyzText.Text = "XYZ:";
+            // 
+            // yuvText
+            // 
+            this.yuvText.AutoSize = true;
+            this.yuvText.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.yuvText.ForeColor = System.Drawing.Color.White;
+            this.yuvText.Location = new System.Drawing.Point(521, 70);
+            this.yuvText.Name = "yuvText";
+            this.yuvText.Size = new System.Drawing.Size(36, 17);
+            this.yuvText.TabIndex = 42;
+            this.yuvText.Text = "YUV:";
+            // 
+            // ycbcrText
+            // 
+            this.ycbcrText.AutoSize = true;
+            this.ycbcrText.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ycbcrText.ForeColor = System.Drawing.Color.White;
+            this.ycbcrText.Location = new System.Drawing.Point(508, 96);
+            this.ycbcrText.Name = "ycbcrText";
+            this.ycbcrText.Size = new System.Drawing.Size(49, 17);
+            this.ycbcrText.TabIndex = 43;
+            this.ycbcrText.Text = "YCbCr:";
+            // 
+            // lchText
+            // 
+            this.lchText.AutoSize = true;
+            this.lchText.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lchText.ForeColor = System.Drawing.Color.White;
+            this.lchText.Location = new System.Drawing.Point(522, 122);
+            this.lchText.Name = "lchText";
+            this.lchText.Size = new System.Drawing.Size(35, 17);
+            this.lchText.TabIndex = 44;
+            this.lchText.Text = "LCH:";
             // 
             // main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(29)))), ((int)(((byte)(39)))));
+            this.ClientSize = new System.Drawing.Size(698, 376);
+            this.Controls.Add(this.lchText);
+            this.Controls.Add(this.ycbcrText);
+            this.Controls.Add(this.yuvText);
+            this.Controls.Add(this.xyzText);
+            this.Controls.Add(this.cielabText);
+            this.Controls.Add(this.cmykText);
+            this.Controls.Add(this.hsvText);
+            this.Controls.Add(this.hslText);
+            this.Controls.Add(this.screen);
+            this.Controls.Add(this.xPosition);
+            this.Controls.Add(this.yPosition);
+            this.Controls.Add(this.rgbText);
+            this.Controls.Add(this.copyright);
+            this.Controls.Add(this.hexText);
+            this.Controls.Add(this.selected_B);
+            this.Controls.Add(this.color_list);
+            this.Controls.Add(this.selected_G);
+            this.Controls.Add(this.selected_color);
+            this.Controls.Add(this.selected_R);
+            this.Controls.Add(this.screen_color);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "main";
             this.Text = "Color Picker";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.main_FormClosing);
+            this.Load += new System.EventHandler(this.main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.screen_color)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.screen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.selected_color)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox screen_color;
+        private System.Windows.Forms.PictureBox screen;
+        private System.Windows.Forms.Label xPosition;
+        private System.Windows.Forms.Label yPosition;
+        private System.Windows.Forms.Label rgbText;
+        private System.Windows.Forms.Label copyright;
+        private System.Windows.Forms.Label hexText;
+        private System.Windows.Forms.Label selected_B;
+        private System.Windows.Forms.ListBox color_list;
+        private System.Windows.Forms.Label selected_G;
+        private System.Windows.Forms.PictureBox selected_color;
+        private System.Windows.Forms.Label selected_R;
+        private System.Windows.Forms.Label hslText;
+        private System.Windows.Forms.Label hsvText;
+        private System.Windows.Forms.Label cmykText;
+        private System.Windows.Forms.Timer screen_timer;
+        private System.Windows.Forms.Label cielabText;
+        private System.Windows.Forms.Label xyzText;
+        private System.Windows.Forms.Label yuvText;
+        private System.Windows.Forms.Label ycbcrText;
+        private System.Windows.Forms.Label lchText;
     }
 }
 
--- src/ColorPicker/main.cs
+++ src/ColorPicker/main.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +17,291 @@ namespace ColorPicker
         public main()
         {
             InitializeComponent();
+            screen_timer.Enabled = true;
+            Load += main_Load;
+            FormClosing += main_FormClosing;
+        }
+
+        private delegate void SetColorDelegate(int x, int y, Color color);
+        private Thread thread;
+
+        private void ProcessThread()
+        {
+            while (true)
+            {
+                Point mousePoint = Control.MousePosition;
+                int x = mousePoint.X;
+                int y = mousePoint.Y;
+                Color color = GetMousePointColor(mousePoint);
+                SetColor(x, y, color);
+            }
+        }
+
+        private string GetHexadecimalString(int value)
+        {
+            byte[] byteArray = BitConverter.GetBytes(value);
+            int byteArrayLength = byteArray.Length;
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < byteArrayLength; i++)
+            {
+                stringBuilder.Append(byteArray[i].ToString("X2"));
+            }
+            return stringBuilder.ToString();
+        }
+
+        public (double H, double S, double L) RGBToHSL(int R, int G, int B)
+        {
+            // Normalize the RGB values to the range 0-1
+            double r = R / 255.0;
+            double g = G / 255.0;
+            double b = B / 255.0;
+
+            // Get the max and min RGB values
+            double max = Math.Max(r, Math.Max(g, b));
+            double min = Math.Min(r, Math.Min(g, b));
+
+            // Calculate Lightness (L)
+            double L = (max + min) / 2.0;
+
+            // Calculate Saturation (S)
+            double S = 0;
+            if (max != min)
+            {
+                if (L < 0.5)
+                {
+                    S = (max - min) / (max + min);
+                }
+                else
+                {
+                    S = (max - min) / (2.0 - max - min);
+                }
+            }
+
+            // Calculate Hue (H)
+            double H = 0;
+            if (max == min)
+            {
+                // If max == min, Hue is undefined, so we assign a default value (0 or 360)
+                H = 0;
+            }
+            else if (max == r)
+            {
+                H = (g - b) / (max - min);
+            }
+            else if (max == g)
+            {
+                H = (b - r) / (max - min) + 2;
+            }
+            else if (max == b)
+            {
+                H = (r - g) / (max - min) + 4;
+            }
+
+            // Convert Hue to degrees (0 to 360)
+            H *= 60;
+            if (H < 0)
+            {
+                H += 360;
+            }
+
+            // Round H, S, and L to the first decimal place
+            H = Math.Round(H, 1);
+            S *= 100;
+            S = Math.Round(S, 1);
+            L *= 100;
+            L = Math.Round(L, 1);
+
+            return (H, S, L);
+        }
+
+        public (double H, double S, double V) RGBToHSV(int R, int G, int B)
+        {
+            // Normalize the RGB values to the range 0-1
+            double r = R / 255.0;
+            double g = G / 255.0;
+            double b = B / 255.0;
+
+            // Get the max and min RGB values
+            double max = Math.Max(r, Math.Max(g, b));
+            double min = Math.Min(r, Math.Min(g, b));
+
+            // Calculate Value (V)
+            double V = max;
+
+            // Calculate Saturation (S)
+            double S = 0;
+            if (max != 0)
+            {
+                S = (max - min) / max;
+            }
+
+            // Calculate Hue (H)
+            double H = 0;
+            if (max == min)
+            {
+                H = 0; // If max == min, Hue is undefined, so we assign a default value (0)
+            }
+            else if (max == r)
+            {
+                H = (g - b) / (max - min);
+            }
+            else if (max == g)
+            {
+                H = (b - r) / (max - min) + 2;
+            }
+            else if (max == b)
+            {
+                H = (r - g) / (max - min) + 4;
+            }
+
+            // Convert Hue to degrees (0 to 360)
+            H *= 60;
+            if (H < 0)
+            {
+                H += 360;
+            }
+
+            // Round H, S, and V to the first decimal place
+            H = Math.Round(H, 1);
+            S *= 100;
+            S = Math.Round(S, 1);
+            V *= 100;
+            V = Math.Round(V, 1);
+
+            return (H, S, V);
+        }
+
+        public (double C, double M, double Y, double K) RGBToCMYK(int R, int G, int B)
+        {
+            // Normalize the RGB values to the range 0-1
+            double r = R / 255.0;
+            double g = G / 255.0;
+            double b = B / 255.0;
+
+            // Calculate K (Black)
+            double K = 1 - Math.Max(r, Math.Max(g, b));
+
+            // If K is 1, CMY values will be 0
+            double C = 0;
+            double M = 0;
+            double Y = 0;
+
+            if (K < 1)
+            {
+                C = (1 - r - K) / (1 - K);
+                M = (1 - g - K) / (1 - K);
+                Y = (1 - b - K) / (1 - K);
+            }
+
+            // Convert C, M, Y, K to percentages (0-100)
+            C *= 100;
+            M *= 100;
+            Y *= 100;
+            K *= 100;
+
+            // Round C, M, Y, K to 1 decimal place
+            C = Math.Round(C, 1);
+            M = Math.Round(M, 1);
+            Y = Math.Round(Y, 1);
+            K = Math.Round(K, 1);
+
+            return (C, M, Y, K);
+        }
+
+
+
+
+        private void SetColor(int x, int y, Color color)
+        {
+            if (InvokeRequired)
+            {
+                SetColorDelegate setColorDelegate = new SetColorDelegate(SetColor);
+                Invoke(setColorDelegate, x, y, color);
+            }
+            else
+            {
+                var (HSL_H, HSL_S, HSL_L) = RGBToHSL(color.R, color.G, color.B);
+                var (HSV_H, HSV_S, HSV_V) = RGBToHSV(color.R, color.G, color.B);
+                var (CMYK_C, CMYK_M, CMYK_Y, CMYK_K) = RGBToCMYK(color.R, color.G, color.B);
+
+                this.screen_color.BackColor = color;
+                this.rgbText.Text = "RGB:   " + color.R.ToString() + " , " + color.G.ToString() + " , " + color.B.ToString();
+                this.hexText.Text = "HEX:   " + GetHexadecimalString(color.R).Substring(0, 2) + GetHexadecimalString(color.G).Substring(0, 2) + GetHexadecimalString(color.B).Substring(0, 2);
+                this.hslText.Text = "HSL:   " + HSL_H.ToString() + "°" + " , " + HSL_S.ToString() + "%" +  ", "+ HSL_L.ToString() + "%";
+                this.hsvText.Text = "HSV:   " + HSV_H.ToString() + "°" + " , " + HSV_S.ToString() + "%" + ", " + HSV_V.ToString() + "%";
+                this.cmykText.Text = "CMYK:   " + CMYK_C.ToString() + " , " + CMYK_M.ToString() + " , " + CMYK_Y.ToString() + " , " + CMYK_K.ToString();
+            }
+        }
+
+        private void screen_timer_Tick(object sender, EventArgs e)
+        {
+            Size size = new Size(screen.Width, screen.Height);
+
+            // 두 배 크기의 비트맵 생성
+            Bitmap bitmap = new Bitmap(size.Width * 2, size.Height * 2);
+            Graphics graphics = Graphics.FromImage(bitmap);
+
+            // 원본 화면을 캡처
+            Bitmap original = new Bitmap(size.Width, size.Height);
+            Graphics originalGraphics = Graphics.FromImage(original);
+            originalGraphics.CopyFromScreen(MousePosition.X - 34, MousePosition.Y - 34, 0, 0, size, CopyPixelOperation.SourceCopy);
+
+            // 캡처한 이미지를 두 배 크기로 확대하여 새 이미지에 그리기
+            graphics.DrawImage(original, 0, 0, original.Width * 2, original.Height * 2);
+
+            // 확대된 이미지를 화면에 표시
+            screen.Image = bitmap;
+
+            // 마우스 좌표 표시
+            xPosition.Text = "X: " + MousePosition.X.ToString();
+            yPosition.Text = "Y: " + MousePosition.Y.ToString();
+        }
+
+
+
+        private void screen_Paint(object sender, PaintEventArgs e)
+        {
+            var g = e.Graphics;
+
+            g.DrawLine(Pens.Red, 69, 138, 69, 0);
+            g.DrawLine(Pens.Red, -69, 69, 138, 69);
+            g.DrawLine(Pens.Red, 0, 0, 138, 0);
+            g.DrawLine(Pens.Red, 0, 0, 0, 138);
+            g.DrawLine(Pens.Red, 0, 137, 137, 137);
+            g.DrawLine(Pens.Red, 137, 0, 137, 137);
+        }
+
+        private void screen_color_Paint(object sender, PaintEventArgs e)
+        {
+            var g = e.Graphics;
+
+            g.DrawLine(Pens.Red, 0, 0, screen_color.Size.Width, 0);
+            g.DrawLine(Pens.Red, 0, 0, 0, screen_color.Size.Width);
+            g.DrawLine(Pens.Red, 0, screen_color.Size.Height - 1, screen_color.Size.Width - 1, screen_color.Size.Height - 1);
+            g.DrawLine(Pens.Red, screen_color.Size.Width - 1, 0, screen_color.Size.Width - 1, screen_color.Size.Width - 1);
+        }
+
+        private void main_Load(object sender, EventArgs e)
+        {
+            this.thread = new Thread(new ThreadStart(ProcessThread));
+            this.thread.Start();
+        }
+
+        private void main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (this.thread != null && this.thread.IsAlive)
+            {
+                this.thread.Abort();
+            }
+        }
+
+        private Color GetMousePointColor(Point mousePoint)
+        {
+            Size size = new Size(1, 1);
+            Bitmap bitmap = new Bitmap(1, 1);
+            Graphics graphics = Graphics.FromImage(bitmap);
+            graphics.CopyFromScreen(mousePoint.X, mousePoint.Y, 0, 0, size);
+            return bitmap.GetPixel(0, 0);
         }
     }
 }
